f3b13441fe9b8dc0084e79d69d01851e000d7974
alex <alexandr.svetliy@icloud.com>
2026-05-11 20:58:21 -0600

    auto-focus

--- app/files/forms.py
+++ app/files/forms.py
@@ -25,6 +25,7 @@ class FolderCreateForm(forms.ModelForm):
                 'class': 'input input-bordered w-full',
                 'placeholder': 'Folder name',
                 'maxlength': 128,
+                'autofocus': 'autofocus',
             }),
         }
 
@@ -36,6 +37,7 @@ class ItemRenameForm(forms.Form):
             'class': 'input input-bordered w-full',
             'placeholder': 'Name',
             'maxlength': 128,
+            'autofocus': 'autofocus',
         }),
     )
 
@@ -48,25 +50,24 @@ class ItemMoveForm(forms.Form):
         }),
     )
 
+    def get_descendant_folder_ids(self, folder):
+        ids = set()
+        for child in folder.children.all():
+            ids.add(child.id)
+            ids.update(self.get_descendant_folder_ids(child))
+        return ids
+
     def __init__(self, *args, user, excluded_folder=None, **kwargs):
         super().__init__(*args, **kwargs)
         folders = Folder.objects.filter(user=user, deleted_at__isnull=True).order_by('name')
         excluded_ids = set()
         if excluded_folder is not None:
-            excluded_ids = get_descendant_folder_ids(excluded_folder)
+            excluded_ids = self.get_descendant_folder_ids(excluded_folder)
             excluded_ids.add(excluded_folder.id)
-        choices = [('', 'Root')]
+        choices = [('', 'root')]
         choices.extend(
             (str(folder.id), folder.name)
             for folder in folders
             if folder.id not in excluded_ids
         )
         self.fields['destination'].choices = choices
-
-
-def get_descendant_folder_ids(folder):
-    ids = set()
-    for child in folder.children.all():
-        ids.add(child.id)
-        ids.update(get_descendant_folder_ids(child))
-    return ids
